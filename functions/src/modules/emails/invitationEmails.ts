e4faffbc356d415c74d450a089b7699863de7bfe
Rajdeep Sengupta <rajdipgupta019@gmail.com>
2023-11-02 22:22:08 +0530

    invite emails added

--- functions/src/modules/emails/invitationEmails.ts
+++ functions/src/modules/emails/invitationEmails.ts
@@ -1,7 +1,5 @@
 import * as functions from "firebase-functions";
-import * as logger from "firebase-functions/logger";
 import { sendEmail } from "../../packages/sendEmail";
-import * as admin from "firebase-admin";
 
 export const sendWorkspaceInviteEmail = functions.firestore
   .document("workspaces/{workspaceId}/collaborators/{collaboratorsId}")
